40a0d38ccff56b3623d00fe2b98a770eb6baf636
qithoniq <119659334+qithoniq@users.noreply.github.com>
2023-04-20 00:11:25 +0300

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -12,20 +12,20 @@ async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
     await bot.send_message(
         chat_id=msg.chat.id,
-        text=f"""𝖧𝖤𝖸 {msg.from_user.mention},
+        text=f"""❲ 𝖧𝖤𝖸 ❳ {msg.from_user.mention},
 
-𝖳𝖧𝖨𝖲 𝖨𝖲 {me2}
-𝖠𝖭 𝖮𝖯𝖤𝖭 𝖲𝖮𝖴𝖱𝖢𝖤 𝖲𝖤𝖲𝖲𝖨𝖮𝖭 𝖡𝖮𝖳 𝖦𝖤𝖭𝖤𝖱 𝖶𝖱𝖨𝖳𝖳𝖤𝖭 𝖨𝖭 𝖯𝖸𝖱𝖮𝖦𝖱𝖠𝖬
+❲ 𝖳𝖧𝖨𝖲 𝖨𝖲 ❳ {me2}
+❲ 𝖠𝖭 𝖮𝖯𝖤𝖭 𝖲𝖮𝖴𝖱𝖢𝖤 𝖲𝖤𝖲𝖲𝖨𝖮𝖭 𝖡𝖮𝖳 𝖦𝖤𝖭𝖤𝖱 𝖶𝖱𝖨𝖳𝖳𝖤𝖭 𝖨𝖭 𝖯𝖸𝖱𝖮𝖦𝖱𝖠𝖬 ❳
 
-𝖬𝖠𝖣𝖤 𝖶𝖨𝖳𝖧 𝖡𝖸 ︙ [𝖲𝖮𝖱𝖴𝖢𝖤 𝖯𝖱𝖮𝖦𝖱𝖠𝖬𝖬𝖤𝖱](tg://user?id={OWNER_ID}) """,
+❲ 𝖬𝖠𝖣𝖤 𝖶𝖨𝖳𝖧 𝖡𝖸 ❳ ︙ [❲ 𝖲𝖮𝖱𝖴𝖢𝖤 𝖯𝖱𝖮𝖦𝖱𝖠𝖬𝖬𝖤𝖱 ❳](tg://user?id={OWNER_ID}) """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(text="GEMERATE SESSION", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("𝖲𝖮𝖱𝖴𝖢𝖤 𝖣𝖱𝖠𝖦𝖮", url="https://t.me/Drago_dr"),
-                    InlineKeyboardButton("𝖲𝖮𝖱𝖴𝖢𝖤 𝖯𝖱𝖮𝖦𝖱𝖠𝖬𝖬𝖤𝖱", user_id=OWNER_ID)
+                    InlineKeyboardButton("❲ 𝖲𝖮𝖱𝖴𝖢𝖤 𝖣𝖱𝖠𝖦𝖮 ❳", url="https://t.me/Drago_dr"),
+                    InlineKeyboardButton("❲ 𝖲𝖮𝖱𝖴𝖢𝖤 𝖯𝖱𝖮𝖦𝖱𝖠𝖬𝖬𝖤𝖱 ❳", user_id=OWNER_ID)
                 ]
             ]
         ),
